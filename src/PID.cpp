83c2af085e6e6235c604db85181b830cbdbdb78a
Amit Jain <jainamit.iitd@gmail.com>
2017-05-18 10:30:17 -0700

    PID Controller project.

---
+++ src/PID.cpp
@@ -0,0 +1,39 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) {
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
+    p_error = 0.0;
+    i_error = 0.0;
+    d_error = 0.0;
+    error_set = false;
+    prev_cte = 0.0;
+}
+
+void PID::UpdateError(double cte) {
+    if (!error_set) {
+        error_set = true;
+        prev_cte = cte;
+    }
+    double diff_cte = cte - prev_cte;
+    p_error = cte;
+    d_error = diff_cte;
+    i_error += cte; 
+    prev_cte = cte;            
+}
+
+double PID::TotalError() {
+    return Kp * p_error + Kd * d_error + Ki * i_error;
+}
+
